08d0ca6a18b5ea50c62f41115b49c0781973ebd7
Torsten Krah <krah.tm@gmail.com>
2026-06-09 15:34:41 +0200

    WIP

--- server/src/main/java/com/example/demo/scope/ScopeConfig.java
+++ server/src/main/java/com/example/demo/scope/ScopeConfig.java
@@ -1,11 +1,11 @@
 package com.example.demo.scope;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class ScopeConfig {
--- shell/demo-shell.sh
+++ shell/demo-shell.sh
@@ -3,4 +3,4 @@ _DEBUG_ARGS=""
 if [ -n "$DEBUG" ]; then
   _DEBUG_ARGS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 fi
-java -jar -Xms1G -Xmx1G -XX:MaxMetaspaceSize=384M $_DEBUG_ARGS build/libs/shell.jar "$@"
+java --enable-native-access=ALL-UNNAMED -jar -Xms1G -Xmx1G -XX:MaxMetaspaceSize=384M $_DEBUG_ARGS build/libs/shell.jar "$@"
